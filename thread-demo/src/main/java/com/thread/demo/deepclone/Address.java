45d30f6e89944a8a36728e1db35061bbbe7dc96d
李东升 <lidongsheng@bmsoft.com.cn>
2020-06-16 16:03:51 +0800

    callable 使用

---
+++ thread-demo/src/main/java/com/thread/demo/deepclone/Address.java
@@ -0,0 +1,27 @@
+package com.thread.demo.deepclone;
+
+/**
+ * @author ：Mr.Li
+ * @date ：Created in 2020/4/22 22:15
+ */
+public class Address {
+
+    private String country;
+    private String province;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+}
