0fcccea12fc4ab0a419fd69f466430a97f807023
李东升 <lidongsheng@bmsoft.com.cn>
2020-06-16 17:34:05 +0800

    callable 使用0.1

--- thread-demo/src/main/java/com/thread/demo/ThreadPool.java
+++ thread-demo/src/main/java/com/thread/demo/ThreadPool.java
@@ -1,19 +1,27 @@
 package com.thread.demo;
 
+import sun.misc.Unsafe;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author ：Mr.Li
  * @date ：Created in 2020/3/31 14:49
  */
 public class ThreadPool /*implements Runnable*/ {
 
+    private static final Unsafe unsafe = Unsafe.getUnsafe();
+
       int a;
 
     public static void main(String[] args) {
       /*  ExecutorService executorService = Executors.newFixedThreadPool(10);
         ForkJoinPool pool = ForkJoinPool.commonPool();
-
         executorService.execute(new ThreadPool());*/
+
         ThreadPool threadPool = new ThreadPool();
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
         threadPool.add();
     }
 
