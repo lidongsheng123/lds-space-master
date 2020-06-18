4257a68c39dfe413531e812ae8e6b8146534aabb
李东升 <lidongsheng@bmsoft.com.cn>
2020-06-18 16:51:49 +0800

    基于消息可靠性投递的分布式事务解决方案init

---
+++ tcc-demo/src/main/java/com/tcc/demo/TccDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TccDemoApplication.class, args);
+    }
+
+}
---
+++ tcc-demo/src/test/java/com/tcc/demo/TccDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
